1fdcdb0cef0b0a20f0d2defc2e42b295ec0c354f
Chris Ham <github@greenhamsolutions.com>
2014-04-07 08:08:28 -0700

    use local storage for ingredient densities, support basic searching via ingredient input

--- assets/js/v2w.js
+++ assets/js/v2w.js
@@ -1,16 +1,56 @@
 $(function() {
   var densities;
 
+  if (localStorage && localStorage.getItem('v2w-densities')) {
+    densities = JSON.parse(localStorage.getItem('v2w-densities'));
+  } else {
+    densities = initData();
+  }
+
+  console.log(densities);
+
+  if (densities.length > 0) {
+    startApp(densities);
+  } else {
+    // @todo handle gracefully
+  }
+});
+
+function initData()
+{
   $.post(
     'server.php',
     {action: "initData"},
     function (data) {
       if (data.status == 'success') {
         densities = data.data;
+        if (localStorage) {
+          localStorage.setItem('v2w-densities', JSON.stringify(densities));
+        }
+        return densities;
       } else if (data.status == 'error') {
         console.error(data.message);
+        return [];
       }
     },
     'json'
   );
-});
+}
+
+function startApp(densities)
+{
+  var ingredientResults = [];
+
+  $('input#ingredient').on('keyup', function (e) {
+    $el = $(this);
+    if ($el.val().length > 2) {
+      // search through densities and find matching ingredients
+      $searchName = event.currentTarget.value;
+      var regString = $searchName;
+      var regex = new RegExp(regString, "i");
+      ingredientResults = _.find(densities, function(item) {
+        return regex.test(item.name);
+      });
+    }
+  });
+}
