9414a8592f985403f77b790ff33e22ab65685fd1
Chris Ham <github@greenhamsolutions.com>
2014-04-09 21:31:01 -0700

    implemented conversion for all volume units to grams

--- assets/js/v2w.js
+++ assets/js/v2w.js
@@ -42,6 +42,18 @@ function startApp(densities)
   var $unitSelect = $('select#unit');
   var $convertBtn = $('button#convert-btn');
 
+  var unitConversions = {
+    "dl":   100,
+    "l":    1000,
+    "t":    5,
+    "T":    15,
+    "floz": 29.6,
+    "c":    237,
+    "pt":   473,
+    "qt":   946,
+    "gal":  3785
+  };
+
   var ingredientNames = _.map(densities, function (item) {
     return item.name;
   });
@@ -56,11 +68,24 @@ function startApp(densities)
     var amount = $amountInput.val();
     var unit = $unitSelect.children(':selected').val();
 
-    // find g/ml for this ingredient
+    // find density for this ingredient
     var density = _.find(densities, function (item) {
       return item.name == ingredient;
     });
 
-    alert("Density is " + density.g_ml + "g/ml");
+    if (density === undefined) {
+      alert("No match found for '"+ingredient+"'");
+    }
+
+    // do conversion based on units selected
+    var grams;
+    if (density.g_whole === null) {
+      grams = amount * density.g_ml * unitConversions[unit];
+    } else {
+      grams = amount * density.g_whole;
+      unit = "units";
+    }
+
+    alert(amount + " " + unit + " of " + ingredient + " = " + grams.toFixed(2) + " grams");
   });
 }
