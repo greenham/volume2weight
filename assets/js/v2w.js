8c1f524f75044318c3c35ecf5cb4c041d29d6931
Chris Ham <github@greenhamsolutions.com>
2014-04-09 22:28:21 -0700

    big UI improvements

--- assets/js/v2w.js
+++ assets/js/v2w.js
@@ -38,11 +38,13 @@ function initData()
 function startApp(densities)
 {
   var $ingredientInput = $('input#ingredient');
-  var $amountInput = $('input#amount');
-  var $unitSelect = $('select#unit');
-  var $convertBtn = $('button#convert-btn');
+  var $amountInput     = $('input#amount');
+  var $unitSelect      = $('select#unit');
+  var $convertBtn      = $('button#convert-btn');
+  var $resetBtn        = $('button#reset-btn');
 
   var unitConversions = {
+    "ml":   1,
     "dl":   100,
     "l":    1000,
     "t":    5,
@@ -54,6 +56,8 @@ function startApp(densities)
     "gal":  3785
   };
 
+  var g_lb = 453;
+
   var ingredientNames = _.map(densities, function (item) {
     return item.name;
   });
@@ -63,10 +67,15 @@ function startApp(densities)
     minLength: 3
   });
 
-  $convertBtn.click(function (e) {
+  var doConversion = function (e) {
+    var $el = $(this);
     var ingredient = $ingredientInput.val();
     var amount = $amountInput.val();
     var unit = $unitSelect.children(':selected').val();
+
+    if (ingredient.length === 0 || amount.length === 0) {
+      return false;
+    }
 
     // find density for this ingredient
     var density = _.find(densities, function (item) {
@@ -86,6 +95,24 @@ function startApp(densities)
       unit = "units";
     }
 
-    alert(amount + " " + unit + " of " + ingredient + " = " + grams.toFixed(2) + " grams");
+    if (grams !== null)
+    {
+      var pounds = grams / g_lb;
+      $el.parent().siblings('td.grams').html(grams.toFixed(2) + "g");
+      $el.parent().siblings('td.pounds').html(pounds.toFixed(2) + "lb");
+    }
+  };
+
+  /*$convertBtn.click(doConversion);*/
+  $ingredientInput.blur(doConversion);
+  $amountInput.blur(doConversion);
+  $unitSelect.change(doConversion);
+
+  $resetBtn.click(function (e) {
+    $(this).parent().siblings('td.grams,td.pounds').html("");
+    $ingredientInput.val("");
+    $amount.val("");
   });
+
+  $ingredientInput.focus();
 }
