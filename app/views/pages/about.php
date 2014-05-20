4b8f3fde3854d7ceefdfad1d81d2ad331a55796d
Chris Ham <github@greenhamsolutions.com>
2014-05-20 06:36:53 -0700

    credit where credit is due

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -2,6 +2,8 @@
     <h1>About <?= $config['brand_name']; ?></h1>
 
     <p>
-
+        This web application was inspired by
+        <a href="http://blog.khymos.org/2014/01/23/volume-to-weight-calculator-for-the-kitchen/" target="_blank">a spreadsheet created by Martin Lersch</a>,
+        who runs the <a href="http://blog.khymos.org/"  target="_blank">Khymos blog.
     </p>
 </div>
