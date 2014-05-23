3afb3cc13522d3ff9da1012dcbe4534c3fd8851f
Chris Ham <github@greenhamsolutions.com>
2014-05-22 20:39:12 -0700

    kitchen scales page

---
+++ app/views/pages/kitchen_scales.php
@@ -0,0 +1,22 @@
+<div class="well text-center">
+    <h1>Kitchen Scales</h1>
+
+    <p class="lead">
+        I like having two scales in my arsenal. One for everyday weighing with a decent capacity,
+        and one with a little higher precision for working with very small amounts.
+    </p>
+
+    <div class="row">
+        <div class="col-md-6">
+            <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=ss_til&ad_type=product_link&tracking_id=volumio-20&marketplace=amazon&region=US&placement=B005QRWEBK&asins=B005QRWEBK&linkId=S3PMSOXPYDINA5TE&show_border=true&link_opens_in_new_window=true"></iframe>
+            <p>MIRA Digital Kitchen Scale (11 lbs [4.99 kg] capacity)</p>
+        </div>
+
+        <div class="col-md-6">
+            <iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=ss_til&ad_type=product_link&tracking_id=volumio-20&marketplace=amazon&region=US&placement=B0012LOQUQ&asins=B0012LOQUQ&linkId=4ASPQMG5THVUHMHN&show_border=true&link_opens_in_new_window=true"></iframe>
+            <p>American Weigh 100g x 0.01g Digital Scale</p>
+        </div>
+    </div>
+</div>
+
+<div class="clearfix"></div>
