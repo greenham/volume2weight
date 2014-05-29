9a03b58deacebf05dadc67756da4685854ac4dc9
Chris Ham <github@greenhamsolutions.com>
2014-05-28 18:59:20 -0700

    fix broken link, add link to github

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -4,6 +4,10 @@
     <p>
         This web application was inspired by
         <a href="http://blog.khymos.org/2014/01/23/volume-to-weight-calculator-for-the-kitchen/" target="_blank">a spreadsheet created by Martin Lersch</a>,
-        who runs the <a href="http://blog.khymos.org/"  target="_blank">Khymos blog.
+        who runs the <a href="http://blog.khymos.org/"  target="_blank">Khymos blog</a>.
+    </p>
+
+    <p>
+        The code behind this site <a href="https://github.com/greenham/volume2weight" target="_blank">can be found on GitHub</a>.
     </p>
 </div>
