44302ff6c422a3042b5d8b70e0bbc19cd0ddc787
Chris Ham <greenham@users.noreply.github.com>
2016-09-29 16:36:14 -0700

    add creative commons license

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,7 +1,10 @@
+<hr>
+
 <div class="footer">
   <div class="col-md-8">
     <? if ($config['ads'] === true): ?>
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- volum.io - Footer (Responsive) -->
     <ins class="adsbygoogle"
          style="display:block"
          data-ad-client="ca-pub-6848900149610746"
@@ -10,9 +13,10 @@
     <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
+    </script>
     <? endif; ?>
   </div>
   <div class="col-md-4">
-    <p class="text-right">&copy; <?= date('Y'); ?> <a href="http://greenhamsolutions.com" target="_blank"><img src="http://greenhamsolutions.com/favicon.ico"> Green Ham Solutions LLC</a></p>
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><small>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</small></a>.
   </div>
 </div>
