c97243c34a85a9631856bfd680fb6d8e7c290d60
Chris Ham <greenham@users.noreply.github.com>
2016-09-30 09:13:18 -0700

    update about page with author

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -2,7 +2,11 @@
     <h1>About <?= $config['brand_name']; ?></h1>
 
     <p>
-        This web application was inspired by
+        This web application was built in April of 2014 by <a href="https://www.linkedin.com/in/chrisham">Chris Ham</a> out of frustration with other conversion sites which don't take into account the density of the ingredient you're trying to convert from volume to weight.
+    </p>
+
+    <p>
+        The data used to drive this comes from
         <a href="http://blog.khymos.org/2014/01/23/volume-to-weight-calculator-for-the-kitchen/" target="_blank">a spreadsheet created by Martin Lersch</a>,
         who runs the <a href="http://blog.khymos.org/"  target="_blank">Khymos blog</a>.
     </p>
