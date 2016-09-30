7b7f9efa8f7af3aab64f5b424bff747ed171e723
Chris Ham <greenham@users.noreply.github.com>
2016-09-30 09:02:49 -0700

    change author meta tag

--- app/views/layout/head.php
+++ app/views/layout/head.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Convert volume measurements to weight for almost any ingredient. Converts US customary units to grams/pounds based on densities of ingredients.">
     <meta name="keywords" content="convert,volume,weight,ingredient,recipe,metric,units,measurement,free">
-    <meta name="author" content="Green Ham Solutions LLC">
+    <meta name="author" content="Chris Ham">
     <base href="/">
     <link rel="shortcut icon" href="assets/img/favicon.ico">
 
